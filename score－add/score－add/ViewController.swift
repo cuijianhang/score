b45062f53e6f82cc4150d690936c15129490bec8
cuijianhang <a5759@mac25.local>
2016-05-26 12:01:01 +0800

    二次修改

--- score－add/score－add/ViewController.swift
+++ score－add/score－add/ViewController.swift
@@ -23,11 +23,23 @@ import UIKit
 
 
 class ViewController: UIViewController {
+    
     //建立数据库
     var db:SQLiteDB!
     
     @IBOutlet var text2: UITextField!
     @IBOutlet var text1: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //获取数据库实例
+        db = SQLiteDB.sharedInstance()
+        //如果表还不存在则创建表（其中uid为自增主键）
+        db.execute("create table if not exists t_user(uid integer primary key,team varchar(20),score varchar(20))")
+        //如果有数据则加载
+        initUser()
+    }
     
     
     var a = 0
@@ -36,6 +48,59 @@ class ViewController: UIViewController {
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var label1: UILabel!
 
+    //保存
+    @IBAction func button5(sender: AnyObject) {
+        saveUser()
+    }
+    
+    //从SQLite加载数据
+    func initUser() {
+        let data = db.query("select * from t_user")
+        //if data.count > 0 {
+        for var x=0 ; x< date.count ; x++{
+            //获取最后一行数据显示
+            let user = data[data.count-1]
+            record.text! +=(user["team"] as? String)! +
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+    }
+    
+    //保存数据到SQLite
+    func saveUser() {
+        let team = self.text1.text!
+        let score = self.label1.text!
+        //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
+        let sql = "insert into t_user(team,score) values('\(team)','\(score)')"
+        print("sql: \(sql)")
+        //通过封装的方法执行sql
+        let result = db.execute(sql)
+        print(result)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    
+    
+    
+    
+    
+    
+    
     
     //加分
     @IBAction func button4(sender: UIButton) {
@@ -84,63 +149,4 @@ class ViewController: UIViewController {
             label2.text = ("\(b)")
         }
     }
-    
-        
-    
-    
-    
-    
-    
-    
-    
-
-    @IBOutlet var txtUname1:UITextField!
-    @IBOutlet var txtUname2:UITextField!
-    
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
-        db = SQLiteDB.sharedInstance()
-        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
-        initUser()
-    }
-    
-    //save
-    @IBAction func button5(sender: AnyObject)
-    {
-        saveUser()
-    }
-    
-    //从SQLite加载数据
-    func initUser()
-    {
-        let data = db.query("select * from t_user")
-        if data.count > 0
-        {
-            //获取最后一行数据显示
-            let user = data[data.count - 1]
-            txtUname1.text = user["uname1"] as? String
-            txtUname2.text = user["uname2"] as? String
-        }
-    }
-    
-    //保存数据到SQLite
-    func saveUser()
-    {
-        let uname1 = self.txtUname1.text!
-        let uname2 = self.txtUname2.text!
-        //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
-        let sql = "insert into t_user(uname1,uname2) values('\(uname1)','\(uname2)')"
-        print("sql: \(sql)")
-        //通过封装的方法执行sql
-        let result = db.execute(sql)
-        print(result)
-    }
-    
-    override func didReceiveMemoryWarning()
-    {
-        super.didReceiveMemoryWarning()
-    }
-
 }
-
